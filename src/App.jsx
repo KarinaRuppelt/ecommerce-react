9b23958d67970fec197eb30ad6bd8eb1f019f563
Karina Ruppelt <karina@example.com>
2026-02-19 14:45:25 +0000

    Added home page

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Auth from "./pages/Auth.jsx";
+import Checkout from "./pages/Checkout.jsx";
+import NavBar from "./components/NavBar.jsx";
+
+function App() {
+  return (
+    <div className="app">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./App.css";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
+);
